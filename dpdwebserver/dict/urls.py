1974a87c9a03b18126f8326b046fd0e0dc4ee0f3
Falko Strenzke <falkostrenzke@gmail.com>
2023-11-24 10:58:09 +0100

    changed lookup url

--- dpdwebserver/dict/urls.py
+++ dpdwebserver/dict/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path("dpd/", views.render_markdown_file, name="dpd_page"),
     path("velthuis/", views.render_markdown_file, name="velthuis_page"),
     path("dpd/search/", views.SearchEntriesOfDictView.as_view(), name="search_entries_of_dict"),
-    path("dpd/lookup/word/<str:word>", views.lookup_word, name="lookup_word"),
+    path("dpd/lookup/<str:word>", views.lookup_word, name="lookup_word"),
     path("dpd/lookup_gd/word/<str:word>", views.lookup_word, name="lookup_word_gd"),
 ]
