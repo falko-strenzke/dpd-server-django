688325d86810c73e56fb7208ece688f4c02378f4
Falko Strenzke <falkostrenzke@gmail.com>
2024-03-01 09:40:20 +0100

    generalized template location

--- dpdwebserver/dpdwebserver/settings.py
+++ dpdwebserver/dpdwebserver/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -58,7 +59,8 @@ ROOT_URLCONF = 'dpdwebserver.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        "DIRS": ["dpdwebserver/templates/"],
+        #"DIRS": ["dpdwebserver/templates/"],
+        "DIRS": [os.path.join(BASE_DIR, "dpdwebserver/templates/")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
